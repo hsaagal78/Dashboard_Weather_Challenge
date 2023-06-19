159de94ae7d35d0125d001fe2ff3bfc5ef32c10d
hsagal78 <hsagal78@gmail.com>
2023-06-18 23:22:25 -0400

    needs 5 days forcecast display

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var baseWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=cityName&appid=b7697291bbd48724d3d0d47008d23e7e';
+var baseForecast5daysURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=b7697291bbd48724d3d0d47008d23e7e';
 
 var form = document.querySelector('#obtainForecasDate');
 var cityNameInput = document.querySelector('#cityName');
@@ -22,7 +23,7 @@ function createCityArray(cityName) {
   var cityArray = {
     city: cityName
   };
-
+// storege city name
   var storedData = localStorage.getItem(input);
   if (storedData) {
     cityNames = JSON.parse(storedData);
@@ -49,7 +50,7 @@ function showCity() {
 
     for (var i = 0; i < existingCityNames.length; i++) {
       var cityName = existingCityNames[i].city;
-    //   console.log(cityName);
+    
       var button = document.createElement('button');
       button.innerText = cityName;
       shortCut.appendChild(button);
@@ -58,6 +59,40 @@ function showCity() {
 }
 
 showCity();
+////////////////////////////////////////1111111111111111111111111111111///////////////////////////////
+function fetchWeatherData(cityName) {
+    var weatherURL = baseWeatherURL.replace('cityName', cityName);
+    
+    return fetch(weatherURL)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        return data;
+      })
+      .catch(function (error) {
+        console.log('Error:', error);
+      });
+      
+  }
+  
+  function displayWeatherDataFromInput() {
+    //   event.preventDefault();
+    var cityName = cityNameInput.value;
+    fetchWeatherData(cityName)
+      .then(function (data) {
+        console.log(data);
+        displayWeatherDataOnScreen(data);
+        fetchdaysWeatherData(data);
+        
+        
+      });
+  }
+  
+  form.addEventListener('submit', displayWeatherDataFromInput);
+////////////////////////////////////////////////////222222222222222222222222222222//////////////////////////////////////////////////
+
 
 function displayWeatherDataOnScreen(data) {
     var weatherContainer = document.querySelector('#weatherRequest');
@@ -69,8 +104,8 @@ function displayWeatherDataOnScreen(data) {
     var windspeedElement = document.createElement('p');
   
   var cityName = data.name;
-//   console.log(data);
-  var temperature = data.main.temp;
+  console.log(data);
+  var temperature = Math.round(data.main.temp)
   var description = data.weather[0].description;
   var windSpeed = data.wind.speed;
   var humidity = data.main.humidity;
@@ -97,7 +132,7 @@ function displayWeatherDataOnScreen(data) {
  
 
   cityNameElement.textContent = cityName;
-  temperatureElement.textContent = 'Temperature: ' + temperature + '°C';
+  temperatureElement.textContent = 'Temperature: ' + temperature + 'F°';
   descriptionElement.textContent = 'Description: ' + description;
   windspeedElement.textContent = 'Speed Wind: ' + windSpeed + 'mph';
   humidityElement.textContent = 'Humidity: ' +  humidity + '%';
@@ -115,176 +150,71 @@ function displayWeatherDataOnScreen(data) {
   weatherContainer.appendChild(humidityElement);
   weatherContainer.appendChild(timeZoneElement);
 }
+//////////////////////////////////////////////////////33333333333333333333333333333333///////////////////////////////////
 
-function fetchWeatherData(cityName) {
-  var weatherURL = baseWeatherURL.replace('cityName', cityName);
-  return fetch(weatherURL)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      return data;
-    })
-    .catch(function (error) {
-      console.log('Error:', error);
-    });
-}
-
-function displayWeatherDataFromInput(event) {
-    event.preventDefault();
-  var cityName = cityNameInput.value;
-  fetchWeatherData(cityName)
-    .then(function (data) {
-      displayWeatherDataOnScreen(data);
-      fetch5daysWeatherData(data)
-      
-    });
-}
-
-form.addEventListener('submit', displayWeatherDataFromInput);
-/////////////////////////////////////////////////////////////////////////////
 
 var baseForecast5daysURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=b7697291bbd48724d3d0d47008d23e7e';
 
 
 
-function fetch5daysWeatherData(data) {
-    console.log('hola',data)
-    var lat =data.coord.lat;
-    var lon=data.coord.lon;
-    var fivedaysURL = baseForecast5daysURL.replace('{lat}', lat).replace('{lon}', lon);
 
+function fetchdaysWeatherData(data) {
+    console.log('entran cordenadas',data);
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    var fivedaysURL = baseForecast5daysURL.replace('{lat}', lat).replace('{lon}', lon);
+  
     return fetch(fivedaysURL)
       .then(function (res) {
         return res.json();
       })
-      .then(function (data) {
-        console.log(data);
-        return data;
+      .then(function (forecastData) {
+        console.log('five das',forecastData);
+        fiveDaysdisplayForcecast(forecastData);
+        return forecastData;
       })
       .catch(function (error) {
         console.log('Error:', error);
-      });
-  }
+    });
+}
   
-  function display5daysForceCostDataFromInput(event) {
-      event.preventDefault();
-    var cityName = cityNameInput.value;
-    fetch5daysWeatherData(cityName)
-      .then(function (data) {
-        displayWeatherDataOnScreen(data);
-        
-      });
-      
-  }
+//   ////////////////////////////////44444444444444444444444444444444444444444444444444444444444444444444444444
+  function fiveDaysdisplayForcecast(forecastData) {
 
-// function fiveDaysdisplayForcecast (cityName){
-//     var cityName5dayForcecast=cityName;
-//     console.log(cityName5dayForcecast);
-//     for (var i=0; i>)
-//     var weatherContainer = document.querySelector('#forecastRequest1');
-//     var cityNameElement5days = document.createElement('h2');
-//     var temperatureElement5days = document.createElement('p');
-//     var descriptionElement5days = document.createElement('p');
-//     var humidityElement5days = document.createElement('p');
-//     var timeZoneElement5days = document.createElement('p');
+            console.log('one',forecastData);
+            var storeArray = forecastData;
+            console.log('one',forecastData);
+       
+          for (var i = 0; i < 5; i++) {
+            var weatherContainer = document.querySelector('#forecastRequest');
+            var temperatureElementfivedays = document.createElement('p');
+            var descriptionElementfivedays = document.createElement('p');
+            var humidityElementfivedays = document.createElement('p');
+            var timeZoneElement = document.createElement('p');
+  
+            var temperature = Math.round(forecastData.list[i].main.temp);
+            var description = forecastData.list[i].weather[0].description;
+            var windSpeed = forecastData.list[i].wind.speed;
+            var humidity = forecastData.list[i].main.humidity;
+            var timeZone = forecastData.list[i].dt_txt;
+  
+            temperatureElementfivedays.textContent = 'Temperature: ' + temperature + '°C';
+            descriptionElementfivedays.textContent = 'Description: ' + description;
+            windspeedElement.textContent = 'Speed Wind: ' + windSpeed + 'mph';
+            humidityElementfivedays.textContent = 'Humidity: ' + humidity + '%';
+            timeZoneElement.textContent = timeZone;
+  
+            weatherContainer.innerHTML = '';
+  
+            weatherContainer.appendChild(temperatureElementfivedays);
+            weatherContainer.appendChild(descriptionElementfivedays);
+            weatherContainer.appendChild(windspeedElement);
+            weatherContainer.appendChild(humidityElementfivedays);
+            weatherContainer.appendChild(timeZoneElement);
+          }
+          .catch(function (error) {
+            console.log('Error:', error);
+        });
+    }
 
 
-//     var cityName = data.name;
-//       console.log(data);
-//       var temperature = data.main.temp;
-//       var description = data.weather[0].description;
-//       var windSpeed = data.wind.speed;
-//       var humidity = data.main.humidity;
-//       var timeZone = data.timezone;
-//       var currentTime = new Date();
-      
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////
-
-
-    // function displayWeatherDataOnScreen(data) {
-    //     var weatherContainer = document.querySelector('#weatherRequest');
-    //     var cityNameElement = document.createElement('h2');
-    //     var temperatureElement = document.createElement('p');
-    //     var descriptionElement = document.createElement('p');
-    //     var humidityElement = document.createElement('p');
-    //     var timeZoneElement = document.createElement('p');
-    //     var windspeedElement = document.createElement('p');
-      
-    //   var cityName = data.name;
-    //   console.log(data);
-    //   var temperature = data.main.temp;
-    //   var description = data.weather[0].description;
-    //   var windSpeed = data.wind.speed;
-    //   var humidity = data.main.humidity;
-    //   var timeZone = data.timezone;
-    //   var currentTime = new Date();
-      
-    //   // Get the current time in UTC
-    //   var currentUTCTime = currentTime.getTime() + (currentTime.getTimezoneOffset() * 60000);
-      
-    //   // Calculate the new time by adding the timeZone
-    //   var newTime = new Date(currentUTCTime + (timeZone * 1000));
-      
-    //   // Get the hours and minutes in readable format
-    //   var hours = newTime.getHours();
-    //   var minutes = newTime.getMinutes();
-    //   var amPm = hours >= 12 ? 'PM' : 'AM';
-    //   hours = hours % 12;
-    //   hours = hours ? hours : 12;
-      
-    //   // Format the hours and minutes according to the desired format
-    //   var formattedTime = (hours).toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ' ' + amPm;
-      
-    //   // Print the formatted time
-     
-    
-    //   cityNameElement.textContent = cityName;
-    //   temperatureElement.textContent = 'Temperature: ' + temperature + '°C';
-    //   descriptionElement.textContent = 'Description: ' + description;
-    //   windspeedElement.textContent = 'Speed Wind: ' + windSpeed + 'mph';
-    //   humidityElement.textContent = 'Humidity: ' +  humidity + '%';
-    //   timeZoneElement.textContent = formattedTime;
-      
-    
-    
-    //   weatherContainer.innerHTML = '';
-    
-    
-    //   weatherContainer.appendChild(cityNameElement);
-    //   weatherContainer.appendChild(temperatureElement);
-    //   weatherContainer.appendChild(descriptionElement);
-    //   weatherContainer.appendChild(windspeedElement);
-    //   weatherContainer.appendChild(humidityElement);
-    //   weatherContainer.appendChild(timeZoneElement);
-    // }
-    
-    // function fetchWeatherData(cityName) {
-    //   var weatherURL = baseWeatherURL.replace('cityName', cityName);
-    //   return fetch(weatherURL)
-    //     .then(function (res) {
-    //       return res.json();
-    //     })
-    //     .then(function (data) {
-    //       return data;
-    //     })
-    //     .catch(function (error) {
-    //       console.log('Error:', error);
-    //     });
-    // }
-    
-    // function displayWeatherDataFromInput(event) {
-    //     event.preventDefault();
-    //   var cityName = cityNameInput.value;
-    //   fetchWeatherData(cityName)
-    //     .then(function (data) {
-    //       displayWeatherDataOnScreen(data);
-    //     });
-    // }
-    
-    // form.addEventListener('submit', displayWeatherDataFromInput);
